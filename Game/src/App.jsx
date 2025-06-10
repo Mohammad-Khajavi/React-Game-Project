d859348888a0fcb431c56210152fec527e3ac91a
mohammad <151404726+Mohammad-Khajavi@users.noreply.github.com>
2025-06-09 20:50:53 -0400

    feat: Created TimerChallenges componenet.

--- Game/src/App.jsx
+++ Game/src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChange from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChange title="Easy" targetTime={1} />
+        <TimerChange title="Not easy" targetTime={5} />
+        <TimerChange title="Getting tough" targetTime={10} />
+        <TimerChange title="Pros only" targetTime={15} />
+      </div>
     </>
   );
 }
